8561ee2cf996d54408a82db5d0f0d29a08baff49
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-24 15:23:50 -0700

    build: add background.js webworker

---
+++ src/background.js
@@ -0,0 +1,9 @@
+const extensions = 'https://developer.chrome.com/docs/extensions';
+const webstore = 'https://developer.chrome.com/docs/webstore';
+
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['scripts/script.js'],
+  });
+});
