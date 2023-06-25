4ab566083f84df1c9601cf2b9579db3087f06937
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-24 17:57:48 -0700

    feat: add click event handler to make API call

--- src/sidepanel/sidePanel.js
+++ src/sidepanel/sidePanel.js
@@ -1,6 +1,24 @@
+import { explain } from '../scripts/explain';
+
 chrome.runtime.onMessage.addListener(({ name, data }) => {
   if (name !== 'readability-context') return;
   const sidePanel = document.body.querySelector('#sidePanel');
   const selectionParagraphElement = sidePanel.querySelector('#selectedText');
   selectionParagraphElement.textContent = data.value;
 });
+
+function handleClick() {
+  const selectionParagraphElement =
+    document.body.querySelector('#selectedText');
+  const selectedText = selectionParagraphElement.textContent;
+  if (selectedText === '') return;
+
+  explain(selectedText).then((result) => {
+    document.body.querySelector('#explanationText').textContent = result.data;
+  });
+}
+
+// add event listener to button
+document.body
+  .querySelector('#explain-btn')
+  .addEventListener('click', handleClick);
