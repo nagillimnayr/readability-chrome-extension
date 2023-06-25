b06704602ba73d225f4a36f686abb271605dbdd0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-24 19:02:47 -0700

    feat: call API immediately

--- src/sidepanel/sidePanel.js
+++ src/sidepanel/sidePanel.js
@@ -2,6 +2,8 @@
 
 // const { default: explain } = await import('../scripts/explain.js');
 async function explain(text) {
+  // wrap text in quotations
+  const message = `"${text}"`;
   // send text to API
   fetch('http://localhost:3000', {
     method: 'POST',
@@ -9,7 +11,7 @@ async function explain(text) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      message: text,
+      message: message,
     }),
   })
     .then((res) => res.json())
@@ -24,25 +26,31 @@ chrome.runtime.onMessage?.addListener(({ name, data }) => {
   if (name !== 'readability-context') return;
   const selectionParagraphElement = document.querySelector('#selectedText');
   selectionParagraphElement.textContent = data.value;
+
+  // display loading message
+  document.body.querySelector('#explanationText').textContent = 'Loading...';
+
+  // make API call
+  explain(data.value);
 });
 
-function handleClick() {
-  console.log('explain button clicked!');
-  const selectionParagraphElement =
-    document.body.querySelector('#selectedText');
-  const selectedText = selectionParagraphElement.textContent;
-  if (selectedText === '') {
-    console.log('text is null');
-    return;
-  }
+// function handleClick() {
+//   console.log('explain button clicked!');
+//   const selectionParagraphElement =
+//     document.body.querySelector('#selectedText');
+//   const selectedText = selectionParagraphElement.textContent;
+//   if (selectedText === '') {
+//     console.log('text is null');
+//     return;
+//   }
 
-  document.body.querySelector('#explanationText').textContent = 'Loading...';
-  // explain(selectedText).then((result) => {
-  //   document.body.querySelector('#explanationText').textContent = result.data;
-  // });
+//   document.body.querySelector('#explanationText').textContent = 'Loading...';
+//   // explain(selectedText).then((result) => {
+//   //   document.body.querySelector('#explanationText').textContent = result.data;
+//   // });
 
-  explain(selectedText);
-}
+//   explain(selectedText);
+// }
 
-// add event listener to button
-document.querySelector('#explain-btn').addEventListener('click', handleClick);
+// // add event listener to button
+// document.querySelector('#explain-btn').addEventListener('click', handleClick);
