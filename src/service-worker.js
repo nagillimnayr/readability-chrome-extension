db369b5227523d1b206aa4488ce7d8b02fa19da4
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-24 17:04:17 -0700

    build: setup context menu in service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,13 +1,32 @@
 const extensions = 'https://developer.chrome.com/docs/extensions';
 const webstore = 'https://developer.chrome.com/docs/webstore';
 
-chrome.action.onClicked.addListener(async (tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['scripts/script.js'],
+function setupContextMenu() {
+  chrome.contextMenus.create({
+    id: 'readability-context',
+    title: 'Readability',
+    contexts: ['selection'],
+  });
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  setupContextMenu();
+});
+
+chrome.contextMenus.onClicked.addListener((data) => {
+  chrome.runtime.sendMessage({
+    name: 'readability-context',
+    data: { value: data.selectionText },
   });
 });
 
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+// chrome.action.onClicked.addListener(async (tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     files: ['scripts/script.js'],
+//   });
+// });
+
+// chrome.sidePanel
+//   .setPanelBehavior({ openPanelOnActionClick: true })
+//   .catch((error) => console.error(error));
